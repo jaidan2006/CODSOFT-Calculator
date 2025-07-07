fe9c9d60bd0dd5f85c469ea2cf051d786b81777c
jaidan2006 <jaidanray@gmail.com>
2025-07-07 23:02:43 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+def calculate(num1, num2, operation):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Invalid operation"
+
+print("Simple Calculator")
+print("Operations: +  -  *  /")
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+result = calculate(num1, num2, operation)
+print("Result:", result)
